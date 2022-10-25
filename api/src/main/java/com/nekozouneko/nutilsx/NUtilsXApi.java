f571b57a8029ba1e14af9e2281c1fbc0d1c9f9de
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-10-25 21:25:38 +0900

    Update: Apiインターフェース

--- api/src/main/java/com/nekozouneko/nutilsx/NUtilsXApi.java
+++ api/src/main/java/com/nekozouneko/nutilsx/NUtilsXApi.java
@@ -1,4 +1,17 @@
 package com.nekozouneko.nutilsx;
 
 public interface NUtilsXApi {
+
+    /**
+     * Get NUtilsX version
+     * @return version
+     */
+    String getVersion();
+
+    /**
+     * Get NUtilsX platform
+     * @return platform
+     */
+    Platform getPlatform();
+
 }
