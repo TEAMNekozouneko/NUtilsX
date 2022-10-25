d666bb45892ddfad22f54bf833fde533a131ff80
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2022-10-25 21:26:27 +0900

    Update: Util#isPlayer(sender)を実装

--- api/src/main/java/com/nekozouneko/nutilsx/Util.java
+++ api/src/main/java/com/nekozouneko/nutilsx/Util.java
@@ -1,4 +1,26 @@
 package com.nekozouneko.nutilsx;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import org.bukkit.entity.Player;
+
 public class Util {
+
+    private Util() {
+        throw new UnsupportedOperationException("Constructor is disabled");
+    }
+
+    public static boolean isPlayer(org.bukkit.command.CommandSender sender) {
+        if (sender instanceof Player) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPlayer(net.md_5.bungee.api.CommandSender sender) {
+        if (sender instanceof ProxiedPlayer) {
+            return true;
+        }
+        return false;
+    }
+
 }
